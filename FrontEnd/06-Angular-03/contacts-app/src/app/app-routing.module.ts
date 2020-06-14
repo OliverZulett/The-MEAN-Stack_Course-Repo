33a5cac32e7fea737ddeebfbcf56dc2ed8873156
OliverZulett <oliverzulett@gmail.com>
2020-06-14 13:44:51 -0400

    Editado por: SUDO en: 14/06/2020 a horas: 13:44
    contact card

--- FrontEnd/06-Angular-03/contacts-app/src/app/app-routing.module.ts
+++ FrontEnd/06-Angular-03/contacts-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Lista', component: ContactListComponent },
+  { path: 'Contacto', component: ContactComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'Lista' },
+  { path: '**', pathMatch: 'full', redirectTo: 'Lista' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
